6d8a0644ade5ee4c38cb50b2d245c0defc9e343d
julian vega <125841627+julian-vega-blanco@users.noreply.github.com>
2023-03-13 19:30:10 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,349 @@
+const campus = {
+    sedes: {
+        bucaramanga:{
+            direccion: "calle 17 n01 av40",
+            telefono: "3105396040",
+            correo: "campusbug@gmail.com"
+        },
+        bogota:{
+            direccion: "calle 3 n21 av4",
+            telefono: "3128593021",
+            correo: "campusbog@gmail.com"
+        },
+        mexico:{
+            direccion: "calle 134 n32 av47",
+            telefono: "3105307943",
+            correo: "campusmex@gmail.com"
+        },
+        medellin:{
+            direccion: "calle 32 n12 av90",
+            telefono: "31053059342",
+            correo: "campusmed@gmail.com"
+        },
+    },
+
+    bucaramanga:{
+        tainers:{
+            yoiber:{
+                nombre:"yoiber",
+                telefono:"3105994324",
+                teams:"team1, team2",
+                email:"traineryoiber@gmail.com",
+                salon:"apolo",
+                piso:"piso 6",
+                horarios:{
+                    horario1:"6am a 9:15am",
+                    horario2:"10:15am a 1:30pm"
+                },
+                dia:{
+                    semanal:"lunes a viernes",
+                },   
+            },
+            miguel:{
+                nombre:"miguel",
+                telefono:"3167894324",
+                teams:"team1, team2",
+                email:"trainermiguel@gmail.com",
+                salon:"sputnik",
+                piso:"piso 6",
+                horarios:{
+                    horario1:"6am a 9:15am",
+                    horario2:"10:15am a 1:30pm"
+                },
+                dia:{
+                    semanal:"lunes a viernes",
+                },   
+            },
+            vermen:{
+                nombre:"vermen",
+                telefono:"3105993424",
+                teams:"team1, team2",
+                email:"trainervermen@gmail.com",
+                salon:"artemis",
+                piso:"piso 6",
+                horarios:{
+                    horario1:"6am a 9:15am",
+                    horario2:"10:15am a 1:30pm"
+                },
+                dia:{
+                    semanal:"lunes a viernes",
+                },   
+            },
+
+            horarioSer:{
+                trainer1:{
+                    nombre:"Maria caceres",
+                    telefono:"3105893049",
+                    email:"mariacaceres@gmail.com",
+                    piso:"piso 6",
+                    teams:"team1 ,team2",
+                    horarios:{
+                        team1:"11am a 12am",
+                        team2:"8am a 9am"
+                    }
+                },
+                trainer2:{
+                    nombre:"Anguie Zuares",
+                    telefono:"3105893049",
+                    email:"anguiezuares1@gmail.com",
+                    piso:"piso 6",
+                    teams:"team1 ,team2",
+                    horarios:{
+                        team1:"11am a 12am",
+                        team2:"8am a 9am"
+                    }
+                }
+            }
+        },
+        team1:{
+            sputnik:{
+                horarios:"6am a 9:15am",
+                trainer:"miguel",
+                estudiantesSputnik:{
+
+                    estudiante1:{
+                        nombre:"Pepe Perez Caceres",
+                        telefono:"3105669299",
+                        email:"pepeperez@gmail.com",
+                        piso:"piso 6",
+                        nivelAct:{
+                            nivel:"avanzado"
+                        },
+                        locacion:{
+                            barrio:"comunero",
+                            direccion:"calle 17a 7w11",
+                        },
+                        transporte:{
+                            medio:"avion"
+                        }
+                    },
+
+                    estudiante2:{
+                        nombre:"Pepe Perez Caceres",
+                        telefono:"3105669299",
+                        email:"pepeperez@gmail.com",
+                        piso:"piso 6",
+                        nivelAct:{
+                            nivel:"avanzado"
+                        },
+                        locacion:{
+                            barrio:"comunero",
+                            direccion:"calle 17a 7w11",
+                        },
+                        transporte:{
+                            medio:"avion"
+                        }
+                    },
+
+                    estudiante3:{
+                        nombre:"Pepe Perez Caceres",
+                        telefono:"3105669299",
+                        email:"pepeperez@gmail.com",
+                        piso:"piso 6",
+                        nivelAct:{
+                            nivel:"avanzado"
+                        },
+                        locacion:{
+                            barrio:"comunero",
+                            direccion:"calle 17a 7w11",
+                        },
+                        transporte:{
+                            medio:"avion"
+                        }
+                    },
+
+                    estudiante4:{
+                        nombre:"Pepe Perez Caceres",
+                        telefono:"3105669299",
+                        email:"pepeperez@gmail.com",
+                        piso:"piso 6",
+                        nivelAct:{
+                            nivel:"avanzado"
+                        },
+                        locacion:{
+                            barrio:"comunero",
+                            direccion:"calle 17a 7w11",
+                        },
+                        transporte:{
+                            medio:"avion"
+                        }
+                    }
+            
+                }
+            },
+
+            artemis:{
+                horarios:"6am a 9:15am",
+                trainer:"vermen",
+                estudiantesArtemis:{
+                    estudiante1:{
+                        nombre:"Pepe Perez Caceres",
+                        telefono:"3105669299",
+                        email:"pepeperez@gmail.com",
+                        piso:"piso 6",
+                        nivelAct:{
+                            nivel:"avanzado"
+                        },
+                        locacion:{
+                            barrio:"comunero",
+                            direccion:"calle 17a 7w11",
+                        },
+                        transporte:{
+                            medio:"avion"
+                        }
+                    },
+                    estudiante2:{
+                        nombre:"Pepe Perez Caceres",
+                        telefono:"3105669299",
+                        email:"pepeperez@gmail.com",
+                        piso:"piso 6",
+                        nivelAct:{
+                            nivel:"avanzado"
+                        },
+                        locacion:{
+                            barrio:"comunero",
+                            direccion:"calle 17a 7w11",
+                        },
+                        transporte:{
+                            medio:"avion"
+                        }
+                    },
+                    estudiante3:{
+                        nombre:"Pepe Perez Caceres",
+                        telefono:"3105669299",
+                        email:"pepeperez@gmail.com",
+                        piso:"piso 6",
+                        nivelAct:{
+                            nivel:"avanzado"
+                        },
+                        locacion:{
+                            barrio:"comunero",
+                            direccion:"calle 17a 7w11",
+                        },
+                        transporte:{
+                            medio:"avion"
+                        }
+                    },
+                    estudiante4:{
+                        nombre:"Pepe Perez Caceres",
+                        telefono:"3105669299",
+                        email:"pepeperez@gmail.com",
+                        piso:"piso 6",
+                        nivelAct:{
+                            nivel:"avanzado"
+                        },
+                        locacion:{
+                            barrio:"comunero",
+                            direccion:"calle 17a 7w11",
+                        },
+                        transporte:{
+                            medio:"avion"
+                        }
+                    }
+            
+                }
+            },
+
+            apolo:{
+                horarios:"6am a 9:15am",
+                trainer:"yoiber",
+                estudiantesApolo:{
+                    estudiante1:{
+                        nombre:"Pepe Perez Caceres",
+                        telefono:"3105669299",
+                        email:"pepeperez@gmail.com",
+                        piso:"piso 6",
+                        nivelAct:{
+                            nivel:"avanzado"
+                        },
+                        locacion:{
+                            barrio:"comunero",
+                            direccion:"calle 17a 7w11",
+                        },
+                        transporte:{
+                            medio:"avion"
+                        }
+                    },
+                    estudiante2:{
+                        nombre:"Pepe Perez Caceres",
+                        telefono:"3105669299",
+                        email:"pepeperez@gmail.com",
+                        piso:"piso 6",
+                        nivelAct:{
+                            nivel:"avanzado"
+                        },
+                        locacion:{
+                            barrio:"comunero",
+                            direccion:"calle 17a 7w11",
+                        },
+                        transporte:{
+                            medio:"avion"
+                        }
+                    },
+                    estudiante3:{
+                        nombre:"Pepe Perez Caceres",
+                        telefono:"3105669299",
+                        email:"pepeperez@gmail.com",
+                        piso:"piso 6",
+                        nivelAct:{
+                            nivel:"avanzado"
+                        },
+                        locacion:{
+                            barrio:"comunero",
+                            direccion:"calle 17a 7w11",
+                        },
+                        transporte:{
+                            medio:"avion"
+                        }
+                    },
+                    estudiante4:{
+                        nombre:"Pepe Perez Caceres",
+                        telefono:"3105669299",
+                        email:"pepeperez@gmail.com",
+                        piso:"piso 6",
+                        nivelAct:{
+                            nivel:"avanzado"
+                        },
+                        locacion:{
+                            barrio:"comunero",
+                            direccion:"calle 17a 7w11",
+                        },
+                        transporte:{
+                            medio:"avion"
+                        }
+                    }
+            
+                }
+            },
+
+            horarioInglés:{
+                trainer1:{
+                    nombre:"Fabio Alverto",
+                    telefono:"3105893049",
+                    email:"fabioalverto1@gmail.com",
+                    piso:"piso 6",
+                    teams:"team1 ,team2",
+                    horarios:{
+                        team1:"10am a 11am",
+                        team2:"9am a 10am"
+                    }
+                },
+                trainer2:{
+                    nombre:"Diego Alverto",
+                    telefono:"3105893049",
+                    email:"diegoalverto1@gmail.com",
+                    piso:"piso 6",
+                    teams:"team1 ,team2",
+                    horarios:{
+                        team1:"10am a 11am",
+                        team2:"9am a 10am"
+                    }
+                }
+            },
+
+            
+
+                      
+       }          
+    }    
+ }
